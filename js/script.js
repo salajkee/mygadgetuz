26cf8b6734022f5173813ff9d2026d7c32b7ad3c
Salamat <salajkee@mail.ru>
2024-05-08 17:08:24 +0500

    upd

--- js/script.js
+++ js/script.js
@@ -40,3 +40,54 @@ try {
 		});
 	});
 } catch (e) {}
+
+// Modal
+try {
+	const modalTrigger = document.querySelector('[data-modal-cb]');
+	const modal = document.querySelector('.modal-cb');
+	const modalClose = modal.querySelector('.modal__close');
+	const modalBody = modal.querySelector('.modal__body');
+	const modalContent = modal.querySelector('.modal__content');
+	const modalThnks = modal.querySelector('.modal__thnks');
+
+	modalTrigger.addEventListener('click', () => {
+		let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
+		document.body.classList.add('no-scroll');
+		modal.classList.remove('hide');
+		modal.classList.add('show');
+		document.body.style.paddingRight = paddingOffset;
+	});
+
+	modalClose.addEventListener('click', () => {
+		modal.classList.remove('show');
+		modal.classList.add('hide');
+		document.body.style.paddingRight = '0px';
+		document.body.classList.remove('no-scroll');
+	});
+} catch (e) {}
+
+try {
+	const modalTrigger = document.querySelector('[data-modal-vd]');
+	const modal = document.querySelector('.modal-vd');
+	const modalClose = modal.querySelector('.modal__close');
+	const iframe = modal.querySelector('#youtube-iframe');
+
+	modalTrigger.addEventListener('click', () => {
+		let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
+		document.body.classList.add('no-scroll');
+		modal.classList.remove('hide');
+		modal.classList.add('show');
+		document.body.style.paddingRight = paddingOffset;
+	});
+
+	modalClose.addEventListener('click', () => {
+		modal.classList.remove('show');
+		modal.classList.add('hide');
+		document.body.style.paddingRight = '0px';
+		document.body.classList.remove('no-scroll');
+		iframe.contentWindow.postMessage(
+			'{"event":"command","func":"stopVideo","args":""}',
+			'*'
+		);
+	});
+} catch (e) {}
